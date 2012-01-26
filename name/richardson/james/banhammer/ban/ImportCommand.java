86f733f554a415cfca982f7a1093a30d90fa8ce6
James Richardson <me+github@james.richardson.name>
2012-01-26 20:56:50 +0000

    Updated ImportCommand

--- name/richardson/james/banhammer/ban/ImportCommand.java
+++ name/richardson/james/banhammer/ban/ImportCommand.java
@@ -22,49 +22,55 @@ import java.util.Map;
 
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 import name.richardson.james.banhammer.BanHammer;
-import name.richardson.james.banhammer.Command;
-import name.richardson.james.banhammer.util.Logger;
+import name.richardson.james.bukkit.util.command.PlayerCommand;
 
-public class ImportCommand extends Command {
+public class ImportCommand extends PlayerCommand {
 
-  private BanHandler banHandler;
+  public static final String NAME = "import";
+  public static final String DESCRIPTION = "Import bans from banned-players.txt";
+  public static final String PERMISSION_DESCRIPTION = "Allow users to import bans from banned-players.txt";
+  public static final String USAGE = "";
 
+  public static final Permission PERMISSION = new Permission("banhammer.import", ImportCommand.PERMISSION_DESCRIPTION, PermissionDefault.OP);
+  
+  private final BanHandler banHandler;
+  private final Server server;
+  
   public ImportCommand(final BanHammer plugin) {
-    super(plugin);
-    this.name = BanHammer.getMessage("import-command-name");
-    this.description = BanHammer.getMessage("import-command-description");
-    this.usage = BanHammer.getMessage("import-command-usage");
-    this.permission = "banhammer." + this.name;
-    registerPermission(this.permission, this.description, PermissionDefault.OP);
+    super(plugin, ImportCommand.NAME, ImportCommand.DESCRIPTION, ImportCommand.USAGE, ImportCommand.PERMISSION_DESCRIPTION, ImportCommand.PERMISSION);
     this.banHandler = plugin.getHandler();
+    this.server = plugin.getServer();
   }
 
   @Override
-  public void execute(final CommandSender sender, Map<String, String> arguments) {
-    final int totalBans = this.plugin.getServer().getBannedPlayers().size();
-    int imported = this.plugin.getServer().getBannedPlayers().size();
+  public void execute(final CommandSender sender, Map<String, Object> arguments) {
+    final int totalBans = server.getBannedPlayers().size();
+    int imported = 0;
     final long expiryTime = 0;
     final String reason = BanHammer.getMessage("default-import-reason");
     final String senderName = sender.getName();
     
-    for (OfflinePlayer player : this.plugin.getServer().getBannedPlayers()) {
+    for (OfflinePlayer player : server.getBannedPlayers()) {
       if (!this.banHandler.banPlayer(player.getName(), senderName, reason, expiryTime, false)) {
         Logger.info(String.format(BanHammer.getMessage("failed-import"), player.getName()));
-        imported = imported - 1;
-      } 
+      } else {
+        imported++;
+      }
       player.setBanned(false);
     }
     
-    Logger.info(String.format(BanHammer.getMessage("ban-import"), this.getSenderName(sender), imported));
+    logger.info(String.format(BanHammer.getMessage("ban-import"), sender.getName(), imported));
     sender.sendMessage(String.format(ChatColor.GREEN + BanHammer.getMessage("bans-imported"), imported, totalBans));
   }
 
   @Override
-  protected Map<String, String> parseArguments(List<String> arguments) {
+  public Map<String, Object> parseArguments(List<String> arguments) {
     return null;
   }
 
