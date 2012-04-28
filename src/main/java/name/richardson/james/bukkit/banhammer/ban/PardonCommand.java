3aaf7f983c147f67879b84043102f04775da04e4
James Richardson <me+github@james.richardson.name>
2012-04-28 13:52:02 +0100

    Updated PardonCommand, fixes #19

--- src/main/java/name/richardson/james/bukkit/banhammer/ban/PardonCommand.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/ban/PardonCommand.java
@@ -17,74 +17,125 @@
  ******************************************************************************/
 package name.richardson.james.bukkit.banhammer.ban;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 import name.richardson.james.bukkit.banhammer.BanHammer;
 import name.richardson.james.bukkit.banhammer.BanHandler;
 import name.richardson.james.bukkit.banhammer.BanRecord;
-import name.richardson.james.bukkit.util.command.CommandArgumentException;
-import name.richardson.james.bukkit.util.command.CommandPermissionException;
-import name.richardson.james.bukkit.util.command.PlayerCommand;
+import name.richardson.james.bukkit.utilities.command.CommandArgumentException;
+import name.richardson.james.bukkit.utilities.command.CommandPermissionException;
+import name.richardson.james.bukkit.utilities.command.CommandUsageException;
+import name.richardson.james.bukkit.utilities.command.PluginCommand;
+import name.richardson.james.bukkit.utilities.internals.Logger;
 
-public class PardonCommand extends PlayerCommand {
+public class PardonCommand extends PluginCommand {
 
-  public static final String NAME = "pardon";
-  public static final String DESCRIPTION = "Pardon a player";
-  public static final String PERMISSION_DESCRIPTION = "Allow users to pardon a player";
-  public static final String USAGE = "<name>";
-
-  public static final Permission PERMISSION = new Permission("banhammer.pardon", PardonCommand.PERMISSION_DESCRIPTION, PermissionDefault.OP);
-  public static final Permission PERMISSION_ALL = new Permission("banhammer.pardon.all", "Allow users to pardon all bans regardless of who issued it", PermissionDefault.OP);
-
+  /** The logger for this class . */
+  private final static Logger logger = new Logger(PardonCommand.class);
+  
+  /** A reference to the BanHammer API. */
   private final BanHandler handler;
-  private final BanHammer plugin;
+  
+  /** A instance of the Bukkit server. */
+  private final Server server;
+  
+  /** The player who is going to be pardoned */
+  private OfflinePlayer player;
 
   public PardonCommand(final BanHammer plugin) {
-    super(plugin, PardonCommand.NAME, PardonCommand.DESCRIPTION, PardonCommand.USAGE, PardonCommand.PERMISSION_DESCRIPTION, PardonCommand.PERMISSION);
+    super(plugin);
     handler = plugin.getHandler(PardonCommand.class);
     this.plugin = plugin;
-    final Permission wildcard = new Permission(PardonCommand.PERMISSION.getName() + ".*", "Allow a user to pardon all bans.", PermissionDefault.OP);
-    this.plugin.addPermission(wildcard, true);
-    PardonCommand.PERMISSION_ALL.addParent(wildcard, true);
-    this.plugin.addPermission(PardonCommand.PERMISSION_ALL, false);
+    this.server = plugin.getServer();
+    this.registerPermissions();
+  }
+  
+  private void registerPermissions() {
+    final String prefix = plugin.getDescription().getName().toLowerCase() + ".";
+    final String wildcardDescription = String.format(plugin.getMessage("wildcard-permission-description"), this.getName());
+    // create the wildcard permission
+    Permission wildcard = new Permission(prefix + this.getName() + ".*", wildcardDescription, PermissionDefault.OP);
+    wildcard.addParent(plugin.getRootPermission(), true);
+    this.addPermission(wildcard);
+    // create the base permission
+    Permission base = new Permission(prefix + this.getName(), plugin.getMessage("pardoncommand-permission-description"), PermissionDefault.OP);
+    base.addParent(wildcard, true);
+    this.addPermission(base);
+    // add ability to pardon your own bans
+    Permission own = new Permission(prefix + this.getName() + "." + plugin.getMessage("pardoncommand-own-permission-name"), plugin.getMessage("pardoncommand-own-permission-name"), PermissionDefault.OP);
+    own.addParent(base, true);
+    this.addPermission(own);
+    // add ability to pardon the bans of others
+    Permission others = new Permission(prefix + this.getName() + "." + plugin.getMessage("pardoncommand-others-permission-name"), plugin.getMessage("pardoncommand-others-permission-name"), PermissionDefault.OP);
+    others.addParent(base, true);
+    this.addPermission(others);
   }
 
-  @Override
-  public void execute(final CommandSender sender, final Map<String, Object> arguments) throws CommandPermissionException {
-    final String playerName = (String) arguments.get("playerName");
-    final String senderName = sender.getName();
-    final BanRecord record = handler.getPlayerBan(playerName);
+  public void execute(CommandSender sender) throws CommandArgumentException, CommandPermissionException, CommandUsageException {
+    final BanRecord record = handler.getPlayerBan(player.getName());
 
     if (record != null) {
-      if (record.getCreatedBy().equalsIgnoreCase(senderName) || sender.hasPermission(PardonCommand.PERMISSION_ALL)) {
-        handler.pardonPlayer(playerName, senderName, true);
-        sender.sendMessage(String.format(ChatColor.GREEN + "%s has been pardoned.", playerName));
-      } else {
-        throw new CommandPermissionException("You may only pardon your own bans.", PardonCommand.PERMISSION_ALL);
+      
+      if (sender.hasPermission(this.getPermission(3)) && !record.getCreatedBy().equalsIgnoreCase(sender.getName())) {
+        handler.pardonPlayer(player.getName(), sender.getName(), true);
+        player.setBanned(false);
+        sender.sendMessage(this.getSimpleFormattedMessage("pardoncommand-response-message", player.getName()));
+        server.broadcast(this.getFormattedBroadcastMessage(sender.getName()), "banhammer.notify");
+        logger.info(this.getFormattedSummaryMessage(sender.getName()));
+        return;
+      } else if (!record.getCreatedBy().equalsIgnoreCase(sender.getName())) {
+        throw new CommandPermissionException(this.getMessage("pardoncommand-cannot-pardon-others-bans"), this.getPermission(3));
       }
+      
+      if (sender.hasPermission(this.getPermission(2)) && record.getCreatedBy().equalsIgnoreCase(sender.getName())) {
+        handler.pardonPlayer(player.getName(), sender.getName(), true);
+        player.setBanned(false);
+        logger.info(this.getFormattedSummaryMessage(sender.getName()));
+        return;
+      } else if (record.getCreatedBy().equalsIgnoreCase(sender.getName())) {
+        throw new CommandPermissionException(this.getMessage("pardoncommand-cannot-pardon-own-bans"), this.getPermission(3));
+      }
+  
     } else {
-      sender.sendMessage(String.format(ChatColor.YELLOW + "%s is not banned.", playerName));
+      sender.sendMessage(this.getSimpleFormattedMessage("player-is-not-banned", player.getName()));
     }
+    
   }
 
-  @Override
-  public Map<String, Object> parseArguments(final List<String> arguments) throws CommandArgumentException {
-    final Map<String, Object> m = new HashMap<String, Object>();
-
-    try {
-      m.put("playerName", arguments.get(0));
-    } catch (final IndexOutOfBoundsException e) {
-      throw new CommandArgumentException("You must specify a valid player name", "You need to type the whole name.");
+  public void parseArguments(String[] arguments, CommandSender sender) throws CommandArgumentException {
+    if (arguments.length == 0) {
+      throw new CommandArgumentException(this.getMessage("must-specify-a-player"), this.getMessage("name-autocompletion"));
+    } else {
+      this.player = matchPlayer(arguments[0]);
     }
-
-    return m;
+    
+  }
+  
+  private String getFormattedSummaryMessage(String name) {
+    final Object[] arguments = { player.getName(), name };
+    return this.getSimpleFormattedMessage("pardoncommand-summary-result", arguments);
   }
 
+  private String getFormattedBroadcastMessage(String name) {
+    final Object[] arguments = {player.getName(), name };
+    return this.getSimpleFormattedMessage("pardoncommand-broadcast-message", arguments);
+  }
+
+  
+  private OfflinePlayer matchPlayer(final String name) {
+    final List<Player> players = this.server.matchPlayer(name);
+    if (players.isEmpty()) {
+      return server.getOfflinePlayer(name);
+    } else {
+      return players.get(0);
+    }
+  }
+  
 }
