d26d3daa9563f95ab6e4847af0e62098994e9cdb
James Richardson <me+github@james.richardson.name>
2012-04-28 14:24:42 +0100

    Updated CheckCommand

--- src/main/java/name/richardson/james/bukkit/banhammer/ban/CheckCommand.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/ban/CheckCommand.java
@@ -17,94 +17,81 @@
  ******************************************************************************/
 package name.richardson.james.bukkit.banhammer.ban;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 import name.richardson.james.bukkit.banhammer.BanHammer;
 import name.richardson.james.bukkit.banhammer.BanHandler;
 import name.richardson.james.bukkit.banhammer.BanRecord;
-import name.richardson.james.bukkit.util.Time;
-import name.richardson.james.bukkit.util.command.CommandArgumentException;
-import name.richardson.james.bukkit.util.command.CommandPermissionException;
-import name.richardson.james.bukkit.util.command.CommandUsageException;
-import name.richardson.james.bukkit.util.command.PlayerCommand;
+import name.richardson.james.bukkit.utilities.command.CommandArgumentException;
+import name.richardson.james.bukkit.utilities.command.CommandPermissionException;
+import name.richardson.james.bukkit.utilities.command.CommandUsageException;
+import name.richardson.james.bukkit.utilities.command.PluginCommand;
 
-public class CheckCommand extends PlayerCommand {
+public class CheckCommand extends PluginCommand {
 
-  public static final String NAME = "check";
-  public static final String DESCRIPTION = "Check if a player is banned";
-  public static final String PERMISSION_DESCRIPTION = "Allow users to check if a player is banned.";
-  public static final String USAGE = "<name>";
-
-  public static final Permission PERMISSION = new Permission("banhammer.check", CheckCommand.PERMISSION_DESCRIPTION, PermissionDefault.OP);
-
+  /** Reference to the BanHammer API */
   private final BanHandler handler;
+  
+  /** The player who we are going to check and see if they are banned */
+  private OfflinePlayer player;
+  
+  /** A instance of the Bukkit server. */
+  private final Server server;
 
   public CheckCommand(final BanHammer plugin) {
-    super(plugin, CheckCommand.NAME, CheckCommand.DESCRIPTION, CheckCommand.USAGE, CheckCommand.PERMISSION_DESCRIPTION, CheckCommand.PERMISSION);
+    super(plugin);
     handler = plugin.getHandler(CheckCommand.class);
+    server = plugin.getServer();
+    this.registerPermissions();
+  }
+  
+  private void registerPermissions() {
+    final String prefix = this.plugin.getDescription().getName().toLowerCase() + ".";
+    // create the base permission
+    final Permission base = new Permission(prefix + this.getName(), this.getMessage("checkcommand-permission-description"), PermissionDefault.OP);
+    base.addParent(this.plugin.getRootPermission(), true);
+    this.addPermission(base);
   }
 
-  @Override
-  public void execute(final CommandSender sender, final Map<String, Object> arguments) throws CommandArgumentException, CommandPermissionException, CommandUsageException {
-    final String playerName = (String) arguments.get("playerName");
-    final BanRecord ban = handler.getPlayerBan(playerName);
-
-    if (ban != null) {
-      if (ban.isActive()) {
-        sender.sendMessage(String.format(ChatColor.RED + "%s is banned.", playerName));
-        sendBanDetail(sender, ban);
-      } else {
-        sender.sendMessage(String.format(ChatColor.YELLOW + "%s is not banned.", playerName));
-      }
+  public void parseArguments(String[] arguments, CommandSender sender) throws CommandArgumentException {
+    if (arguments.length == 0) {
+      if (sender instanceof ConsoleCommandSender) throw new CommandArgumentException(this.getMessage("must-specify-a-player"), this.getMessage("name-autocompletion"));
+      this.player = (OfflinePlayer) sender;
     } else {
-      sender.sendMessage(String.format(ChatColor.YELLOW + "%s is not banned.", playerName));
+      this.player = matchPlayer(arguments[0]);
     }
-
+    
   }
-
-  @Override
-  public Map<String, Object> parseArguments(final List<String> arguments) throws CommandArgumentException {
-    final Map<String, Object> m = new HashMap<String, Object>();
-
-    try {
-      m.put("playerName", arguments.get(0));
-    } catch (final IndexOutOfBoundsException e) {
-      throw new CommandArgumentException("You must specify a valid player name", "You need to type the whole name.");
+  
+  private OfflinePlayer matchPlayer(final String name) {
+    final List<Player> players = this.server.matchPlayer(name);
+    if (players.isEmpty()) {
+      return server.getOfflinePlayer(name);
+    } else {
+      return players.get(0);
     }
-
-    return m;
   }
-
-  protected void sendBanDetail(final CommandSender sender, final BanRecord ban) {
-    final Date createdDate = new Date(ban.getCreatedAt());
-    final DateFormat dateFormat = new SimpleDateFormat("MMM d");
-    final String createdAt = dateFormat.format(createdDate);
-    sender.sendMessage(String.format(ChatColor.YELLOW + "Banned by %s on %s", ban.getCreatedBy(), createdAt));
-    sender.sendMessage(String.format(ChatColor.YELLOW + "- Reason: %s.", ban.getReason()));
-    switch (ban.getType()) {
-    case PERMENANT:
-      sender.sendMessage(ChatColor.YELLOW + "- Length: Permanent.");
-      break;
-    case TEMPORARY:
-      final Date expiryDate = new Date(ban.getExpiresAt());
-      final DateFormat expiryDateFormat = new SimpleDateFormat("MMM d H:mm a ");
-      final String expiryDateString = expiryDateFormat.format(expiryDate) + "(" + Calendar.getInstance().getTimeZone().getDisplayName() + ")";
-      final Long banTime = ban.getExpiresAt() - ban.getCreatedAt();
-      sender.sendMessage(String.format(ChatColor.YELLOW + "- Length: %s", Time.millisToLongDHMS(banTime)));
-      sender.sendMessage(String.format(ChatColor.YELLOW + "- Expires on: %s", expiryDateString));
-      break;
-    }
+  
+  public void execute(CommandSender sender) throws CommandArgumentException, CommandPermissionException, CommandUsageException {
+    final BanRecord ban = handler.getPlayerBan(player.getName());
+    
+    if (ban != null && ban.isActive()) {
+      BanSummary summary = new BanSummary(plugin, ban);
+      sender.sendMessage(summary.getHeader());
+      sender.sendMessage(summary.getReason());
+      sender.sendMessage(summary.getLength());
+      if (ban.getType() == BanRecord.Type.TEMPORARY) sender.sendMessage(summary.getExpiresAt());
+    } else {
+      sender.sendMessage(this.getSimpleFormattedMessage("checkcommand-player-is-not-banned", player.getName()));
+    } 
   }
 
 }
