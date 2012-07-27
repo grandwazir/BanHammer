c8de585a643f296b2ee89a99ad00645b372d8502
James Richardson <me+github@james.richardson.name>
2012-07-27 19:09:15 +0100

    add event handlers

--- src/main/java/name/richardson/james/bukkit/banhammer/api/BanHammerPlayerPardonedEvent.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/api/BanHammerPlayerPardonedEvent.java
@@ -17,14 +17,26 @@
  ******************************************************************************/
 package name.richardson.james.bukkit.banhammer.api;
 
+import org.bukkit.event.HandlerList;
+
 import name.richardson.james.bukkit.banhammer.persistence.BanRecord;
 
 public class BanHammerPlayerPardonedEvent extends BanHammerPlayerEvent {
 
   private static final long serialVersionUID = 768545683909385614L;
 
+  private static final HandlerList handlers = new HandlerList();
+  
+  public static HandlerList getHandlerList() {
+    return handlers;
+  }
+  
   public BanHammerPlayerPardonedEvent(final BanRecord record, final boolean silent) {
     super(record, silent);
+  }
+  
+  public HandlerList getHandlers() {
+    return handlers;
   }
 
 }
