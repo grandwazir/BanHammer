705f59258421e2b652978a42129ad658473c376a
James Richardson <me+github@james.richardson.name>
2013-06-21 22:07:45 +0100

    Update LimitsCommand

--- src/main/java/name/richardson/james/bukkit/banhammer/ban/LimitsCommand.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/ban/LimitsCommand.java
@@ -1,22 +1,23 @@
 /*******************************************************************************
  * Copyright (c) 2012 James Richardson.
- * 
+ *
  * LimitsCommand.java is part of BanHammer.
- * 
+ *
  * BanHammer is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * BanHammer is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * BanHammer. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package name.richardson.james.bukkit.banhammer.ban;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -31,24 +32,20 @@ import name.richardson.james.bukkit.utilities.formatters.ChoiceFormatter;
 import name.richardson.james.bukkit.utilities.formatters.TimeFormatter;
 
 @ConsoleCommand
-@CommandPermissions(permissions = { "banhammer.limits" })
+@CommandPermissions(permissions = {"banhammer.limits"})
 public class LimitsCommand extends AbstractCommand {
 
 	private final ChoiceFormatter formatter;
-
-	/* The ban limits */
 	private final Map<String, Long> limits;
 
 	public LimitsCommand(final Map<String, Long> limits) {
 		super();
 		this.limits = limits;
-		this.formatter = new ChoiceFormatter();
+		this.formatter = new ChoiceFormatter(this.getClass());
 		this.formatter.setLimits(0, 1, 2);
-		this.formatter.setMessage("notice.limits-header");
+		this.formatter.setMessage("limits-header");
 		this.formatter.setArguments(limits.size());
-		this.formatter.setFormats(this.getMessage("shared.choice.no-limits"), this.getMessage("shared.choice.one-limit"),
-			this.getMessage("shared.choice.many-limits"));
-
+		this.formatter.setFormats("no-limits", "many-limits");
 	}
 
 	public void execute(final List<String> arguments, final CommandSender sender) {
@@ -64,7 +61,8 @@ public class LimitsCommand extends AbstractCommand {
 				} else {
 					colour = ChatColor.RED;
 				}
-				sender.sendMessage(colour + this.getMessage("notice.limits-list-item", limit.getKey(), TimeFormatter.millisToLongDHMS(limit.getValue())));
+				String message = MessageFormat.format(this.getLocalisation().getString("limits-list-item"), limit.getKey(), TimeFormatter.millisToLongDHMS(limit.getValue()));
+				sender.sendMessage(colour + message);
 			}
 		}
 	}
