13d9c60bb97ef3d436db955a3d0d165ec403d997
James Richardson <me+github@james.richardson.name>
2013-06-21 22:24:19 +0100

    update BanCommand

--- src/main/java/name/richardson/james/bukkit/banhammer/ban/BanCommand.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/ban/BanCommand.java
@@ -1,22 +1,24 @@
 /*******************************************************************************
  * Copyright (c) 2012 James Richardson.
- * 
+ *
  * BanCommand.java is part of BanHammer.
- * 
+ *
  * BanHammer is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * BanHammer is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * BanHammer. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package name.richardson.james.bukkit.banhammer.ban;
 
+import java.sql.Timestamp;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,50 +33,38 @@ import org.bukkit.command.TabExecutor;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
-import name.richardson.james.bukkit.banhammer.BanHammer;
-import name.richardson.james.bukkit.banhammer.api.BanHandler;
-import name.richardson.james.bukkit.banhammer.matchers.BanLimitMatcher;
 import name.richardson.james.bukkit.utilities.command.AbstractCommand;
 import name.richardson.james.bukkit.utilities.command.CommandMatchers;
 import name.richardson.james.bukkit.utilities.command.CommandPermissions;
 import name.richardson.james.bukkit.utilities.command.ConsoleCommand;
 import name.richardson.james.bukkit.utilities.formatters.StringFormatter;
 import name.richardson.james.bukkit.utilities.formatters.TimeFormatter;
+import name.richardson.james.bukkit.utilities.localisation.LocalisedCommandSender;
 import name.richardson.james.bukkit.utilities.matchers.OnlinePlayerMatcher;
 
+import name.richardson.james.bukkit.banhammer.BanHammer;
+import name.richardson.james.bukkit.banhammer.api.BanHandler;
+import name.richardson.james.bukkit.banhammer.matchers.BanLimitMatcher;
+
 @ConsoleCommand
-@CommandMatchers(matchers = { OnlinePlayerMatcher.class, BanLimitMatcher.class })
-@CommandPermissions(permissions = { "banhammer.ban", "banhammer.ban.permanent" })
+@CommandMatchers(matchers = {OnlinePlayerMatcher.class, BanLimitMatcher.class})
+@CommandPermissions(permissions = {"banhammer.ban", "banhammer.ban.permanent"})
 public class BanCommand extends AbstractCommand implements TabExecutor {
 
-	/** Reference to the BanHammer API. */
 	private final BanHandler handler;
-
-	/** A list of players who are not allowed to be banned normally */
 	private final List<String> immunePlayers;
-
-	/** The ban limit. */
 	private final Map<String, Long> limits;
-
-	/** The name of the player who we are going to ban. */
-	private OfflinePlayer player;
-
-	/** The reason given for the player's ban. */
-	private String reason;
-
-	/** A instance of the Bukkit server. */
 	private final Server server;
 
-	/** How long in milliseconds to ban the player for. */
+	private OfflinePlayer player;
+	private String reason;
 	private long time;
 
 	/**
 	 * Instantiates a new BanCommand.
-	 * 
-	 * @param plugin
-	 *          the plugin that this command belongs to
-	 * @param limits
-	 *          the registered ban limits to use
+	 *
+	 * @param plugin the plugin that this command belongs to
+	 * @param limits the registered ban limits to use
 	 */
 	public BanCommand(final BanHammer plugin, final Map<String, Long> limits, final List<String> immunePlayers) {
 		super();
@@ -86,13 +76,14 @@ public class BanCommand extends AbstractCommand implements TabExecutor {
 	}
 
 	public void execute(final List<String> arguments, final CommandSender sender) {
+		LocalisedCommandSender localisedCommandSender = new LocalisedCommandSender(sender, this.getLocalisation());
 		if (arguments.isEmpty()) {
-			sender.sendMessage(this.getMessage("error.must-specify-player"));
+			localisedCommandSender.error("must-specify-player");
 		} else {
 			this.player = this.server.getOfflinePlayer(arguments.remove(0));
 			if (arguments.isEmpty()) {
-				sender.sendMessage(this.getMessage("error.must-specify-a-reason"));
-				sender.sendMessage(this.getMessage("notice.reason-hint"));
+				localisedCommandSender.error("must-specify-reason");
+				localisedCommandSender.warning("reason-hint");
 				return;
 			} else {
 				if (arguments.get(0).startsWith("t:")) {
@@ -101,8 +92,8 @@ public class BanCommand extends AbstractCommand implements TabExecutor {
 					this.time = 0;
 				}
 				if (arguments.isEmpty()) {
-					sender.sendMessage(this.getMessage("error.must-specify-a-reason"));
-					sender.sendMessage(this.getMessage("notice.reason-hint"));
+					localisedCommandSender.error("must-specify-reason");
+					localisedCommandSender.warning("reason-hint");
 					return;
 				} else {
 					this.reason = StringFormatter.combineString(arguments, " ");
@@ -110,23 +101,30 @@ public class BanCommand extends AbstractCommand implements TabExecutor {
 			}
 			// now set about banning the player
 			if (this.hasPermission(sender)) {
-				if (!this.handler.banPlayer(this.player.getName(), sender.getName(), this.reason, this.time, true)) {
-					sender.sendMessage(this.getMessage("error.player-already-banned", this.player.getName()));
+				Timestamp timestamp;
+				if (this.time == 0) {
+					timestamp = null;
 				} else {
-					sender.sendMessage(this.getMessage("notice.player-banned", this.player.getName()));
+					timestamp = new Timestamp(System.currentTimeMillis() + this.time);
+				}
+				if (!this.handler.banPlayer(this.player.getName(), sender.getName(), this.reason, timestamp, true)) {
+					localisedCommandSender.error("player-already-banned", this.player.getName());
+				} else {
+					localisedCommandSender.info("player-banned", this.player.getName());
 				}
 			} else {
-				sender.sendMessage(this.getMessage("error.permission-denied"));
+				localisedCommandSender.error("permission-denied");
 			}
 		}
 
 	}
 
 	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] arguments) {
+		LocalisedCommandSender localisedCommandSender = new LocalisedCommandSender(sender, this.getLocalisation());
 		if (this.isAuthorized(sender)) {
 			this.execute(new LinkedList<String>(Arrays.asList(arguments)), sender);
 		} else {
-			sender.sendMessage(this.getMessage("error.permission-denied"));
+			localisedCommandSender.error("permission-denied");
 		}
 		return true;
 	}
@@ -136,11 +134,17 @@ public class BanCommand extends AbstractCommand implements TabExecutor {
 	}
 
 	private boolean hasPermission(final CommandSender sender) {
-		if (this.immunePlayers.contains(this.player.getName()) && !sender.hasPermission("banhammer.ban")) { return false; }
-		if (sender.hasPermission("banhammer.ban.permanent")) { return true; }
+		if (this.immunePlayers.contains(this.player.getName()) && !sender.hasPermission("banhammer.ban")) {
+			return false;
+		}
+		if (sender.hasPermission("banhammer.ban.permanent")) {
+			return true;
+		}
 		for (final String limitName : this.limits.keySet()) {
 			final String node = this.getPermissionManager().listPermissions().get(0).getName() + "." + limitName;
-			if (sender.hasPermission(node) && (this.limits.get(limitName) <= this.time)) { return true; }
+			if (sender.hasPermission(node) && (this.limits.get(limitName) <= this.time)) {
+				return true;
+			}
 		}
 		return false;
 	}
@@ -158,9 +162,9 @@ public class BanCommand extends AbstractCommand implements TabExecutor {
 		if (!this.limits.isEmpty()) {
 			final String parentPermissionName = this.getPermissionManager().listPermissions().get(0).getName();
 			for (final Entry<String, Long> limit : this.limits.entrySet()) {
-				final Permission permission =
-					new Permission(parentPermissionName + "." + limit.getKey(), this.getMessage("misc.limit-permission-description",
-						TimeFormatter.millisToLongDHMS(limit.getValue())), PermissionDefault.OP);
+				String name = parentPermissionName + "." + limit.getKey();
+				String description = MessageFormat.format(this.getLocalisation().getString("limit-permission-description"), TimeFormatter.millisToLongDHMS(limit.getValue()));
+				final Permission permission = new Permission(name, description, PermissionDefault.OP);
 				permission.addParent(parentPermissionName, true);
 				this.getPermissionManager().addPermission(permission);
 			}
