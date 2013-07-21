6b1114d6df1b3409cdd471b5d63d2eda472a7506
James Richardson <me+github@james.richardson.name>
2013-07-21 22:19:57 +0100

    added test resources

--- src/main/test/java/name/richardson/james/bukkit/banhammer/CheckCommandTest.java
+++ src/main/test/java/name/richardson/james/bukkit/banhammer/CheckCommandTest.java
@@ -1,11 +1,15 @@
 package name.richardson.james.bukkit.banhammer;
 
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import name.richardson.james.bukkit.utilities.command.context.PassthroughCommandContext;
 import name.richardson.james.bukkit.utilities.permissions.PermissionManager;
 
 import name.richardson.james.bukkit.banhammer.ban.PlayerRecordManager;
@@ -16,12 +20,16 @@ import static org.mockito.Mockito.mock;
 public class CheckCommandTest extends TestCase {
 
 	private CheckCommand command;
+	private CommandSender commandSender;
 	private PermissionManager permissionManager;
 	private PlayerRecordManager playerRecordManager;
+	private Server server;
 
 	@Test
-	public void testExecute()
+	public void testExecuteNoPlayerProvided()
 	throws Exception {
+		String[] arguments = {"12"};
+		new PassthroughCommandContext(arguments, commandSender, server);
 
 	}
 
@@ -30,6 +38,8 @@ public class CheckCommandTest extends TestCase {
 	throws Exception {
 		permissionManager = mock(PermissionManager.class);
 		playerRecordManager = mock(PlayerRecordManager.class);
+		commandSender = mock(CommandSender.class);
+		server = mock(Server.class);
 		command = new CheckCommand(permissionManager, playerRecordManager);
 	}
 }
