07571eb5bf4724ebe670fe1ad491467fd3f85098
James Richardson <me+github@james.richardson.name>
2014-04-17 20:30:07 +0100

    check in missing file

---
+++ src/main/java/name/richardson/james/bukkit/utilities/command/argument/ReasonPositionalArgument.java
@@ -0,0 +1,21 @@
+package name.richardson.james.bukkit.utilities.command.argument;
+
+import org.bukkit.Server;
+
+import name.richardson.james.bukkit.utilities.command.argument.suggester.OnlinePlayerSuggester;
+import name.richardson.james.bukkit.utilities.command.argument.suggester.Suggester;
+
+import static name.richardson.james.bukkit.banhammer.utilities.localisation.BanHammerLocalisation.*;
+
+public class ReasonPositionalArgument {
+
+	public static Argument getInstance(int position, boolean required) {
+		ArgumentMetadata metadata = new SimpleArgumentMetadata(ARGUMENT_REASON_ID, ARGUMENT_REASON_NAME, ARGUMENT_REASON_DESC, ARGUMENT_REASON_ERROR);
+		if (required) {
+			return new RequiredJoinedPositionalArgument(metadata, position);
+		} else {
+			return new JoinedPositionalArgument(metadata, position);
+		}
+	}
+
+}
