847b788649a3d6c0ff9e90a1e8b287cdf61ddb9e
James Richardson <me+github@james.richardson.name>
2014-08-04 17:39:24 +0100

    move old records into legacy package

--- src/main/java/name/richardson/james/bukkit/banhammer/model/legacy/OldPlayerRecord.java
+++ src/main/java/name/richardson/james/bukkit/banhammer/model/legacy/OldPlayerRecord.java
@@ -15,7 +15,7 @@
  You should have received a copy of the GNU General Public License along with
  BanHammer. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package name.richardson.james.bukkit.banhammer.legacy;
+package name.richardson.james.bukkit.banhammer.model.legacy;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
